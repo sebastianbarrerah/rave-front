e8e1e7b6e4b3cc6bc969fa02684a3d8da32a00c3
Sebastian barrera <sebasbh2008@hotmail.com>
2025-01-12 10:38:52 -0500

    feat: add structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="es" className="bg-slate-600">
+      <body>
+        <div className="w-full py-4 text-slate-50 bg-slate-900"> rave </div>
         {children}
       </body>
     </html>
